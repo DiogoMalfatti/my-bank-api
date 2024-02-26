5f20d187cf486023092aed40d94268c747ec3a2c
diogoMalfatti <diimalfatti@gmail.com>
2024-02-26 15:37:10 -0300

    create R crud get

--- routes/accounts.js
+++ routes/accounts.js
@@ -5,17 +5,29 @@ const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
+// metodo Create 'C' do crud
 router.post("/", async (require, response) => {
   try {
     let account = require.body;
-    let data = JSON.parse(await readFile("accounts.json"));
+    const data = JSON.parse(await readFile(global.fileName));
 
     account = { id: data.nextId++, ...account };
     data.accounts.push(account);
 
-    await writeFile("accounts.json", JSON.stringify(data, null, 2));
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
     response.send(account);
+  } catch (err) {
+    response.status(400).send({ error: err.message });
+  }
+});
+
+// metodo Read 'R' do crud
+router.get("/", async (require, response) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextId;
+    response.send(data);
   } catch (err) {
     response.status(400).send({ error: err.message });
   }
